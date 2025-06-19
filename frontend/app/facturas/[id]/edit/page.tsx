dd48935b3f805e8619fa5cc5e1caab4fd0a5a241
Yeray Boix <yerayboix@gmail.com>
2025-06-19 18:27:56 +0200

    feat: WIP de editar factura.

---
+++ frontend/app/facturas/[id]/edit/page.tsx
@@ -0,0 +1,36 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import { Suspense } from "react";
+import FacturaFormWrapper from "./factura-form-wrapper";
+
+export default async function EditFacturaPage(props: { params: Promise<{ id: string }> }) {
+  const { id } = await props.params;
+
+
+  return(
+    <div className="flex flex-1 flex-col gap-4 p-6 py-10">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/facturas">Facturas</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href={`/facturas/${id}`}>Factura</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Editar Factura</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <FacturaFormWrapper id={id} />
+      </Suspense>
+    </div>
+  )
+}
